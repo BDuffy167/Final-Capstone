5adbe9b861151554a7964886a9ec3d8250398212
Brandon Duffy <b.duffy.167@gmail.com>
2021-08-03 13:23:26 -0400

    progress? broken?

--- vue/src/views/Books.vue
+++ vue/src/views/Books.vue
@@ -1,29 +1,45 @@
 <template>
   <main>
     <h1>Book List</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th> <!-- firstname lastname concat -->
+          <th>Minutes Read</th>
+          <th>Finished?</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for=
+      </tbody>
+    </table>
   </main>
 </template>
 
 <script>
-import bookservice from "../services/BookService.js";
+import BookService from "../services/BookService.js";
+import Book from '../components/Book.vue';
 
 export default {
   name: "books",
+  components: {
+    Book
+  },
   data() {
     return {};
   },
-    created() {
-      bookservice
-        .get(this.$store.state.user.userId)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((response) => {
-          console.error(response);
-        });
-    },
-  methods: {
+  created() {
+    BookService
+      .get(this.$store.state.user.userId)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((response) => {
+        console.error(response);
+      });
   },
+  methods: {},
 };
 </script>
 
