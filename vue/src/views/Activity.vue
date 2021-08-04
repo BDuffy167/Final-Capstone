78633024ead19972f561e5c68915e1373a19c609
Brandon Duffy <b.duffy.167@gmail.com>
2021-08-04 13:31:31 -0400

    prelim styling experiments

--- vue/src/views/Activity.vue
+++ vue/src/views/Activity.vue
@@ -1,24 +1,27 @@
 <template>
   <main>
     <h1>Activity List</h1>
-    <table>
+    <table class="readingActivityTable">
       <thead>
         <tr>
           <th>Title</th>
-          <th>Author</th><!-- firstname lastname concat -->
+          <th>Author</th>
+          <!-- firstname lastname concat -->
           <th>Time Read</th>
           <th>Format Type</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="activity of allActivity" v-bind:key="activity.logID">
-        <td>{{activity.loggedBook.title}}</td>
-       <td>{{activity.loggedBook.authorFirstName}} {{activity.loggedBook.authorLastName}}</td>
-        <td>{{activity.timeRead}}</td>
-       <td>{{activity.formatType}}</td>
+          <td>{{ activity.loggedBook.title }}</td>
+          <td>
+            {{ activity.loggedBook.authorFirstName }}
+            {{ activity.loggedBook.authorLastName }}
+          </td>
+          <td>{{ activity.timeRead }}</td>
+          <td>{{ activity.formatType }}</td>
         </tr>
-     </tbody>
-
+      </tbody>
     </table>
   </main>
 </template>
@@ -30,22 +33,21 @@ import BookService from "../services/BookService.js";
 export default {
   name: "activity",
   components: {
-   // BookList
+    // BookList
   },
   computed: {
     allActivity() {
       return this.$store.state.readingLog;
-    }
+    },
   },
   data() {
     return {};
   },
   created() {
-    BookService
-      .get(this.$store.state.user.userId)
+    BookService.get(this.$store.state.user.userId)
       .then((response) => {
         console.log(response);
-        this.$store.commit('SET_READINGLOG', response.data);
+        this.$store.commit("SET_READINGLOG", response.data);
       })
       .catch((response) => {
         console.error(response);
@@ -56,4 +58,21 @@ export default {
 </script>
 
 <style>
+div {
+  /* background-color: aqua; */
+}
+h1 {
+  text-align: center;
+}
+.readingActivityTable  {
+margin-left: auto;
+margin-right: auto;
+}
+td, th {
+  padding: .5em;
+  text-align: center;
+}
+th {
+  border: 2px solid black;
+}
 </style>
